72ac5868f7f699b65f937c29a75da0e3e01c36f5
0shuvo0 <seriouslolz1@gmail.com>
2023-01-26 12:40:54 +0600

    added anim

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let frameCount = 0
+
+
 class Cursor{
    constructor(){
     const cursorEl = document.createElement('img')
@@ -24,6 +27,10 @@ class Cursor{
 
     document.body.appendChild(cursorEl)
 
+    this.animateDx = 1
+    this.animateDy = 1
+    this.animateAlt = parseInt(Math.random() * 150) + 150
+
     this.el = cursorEl
    }
 
@@ -34,6 +41,14 @@ class Cursor{
     this.el.style.top = this.posY + 'px'
     this.el.style.left = this.posX + 'px'
    }
+
+   animate(){
+    if(frameCount % this.animateAlt === 0){
+        if(Math.random() > .5) this.animateDx = -this.animateDx
+        if(Math.random() > .5) this.animateDy = -this.animateDy
+    }
+    this.updatedPos(this.animateDx, this.animateDy)
+   }
 }
 
 const cursors = []
@@ -41,6 +56,17 @@ const CURSOR_COUNT = 200
 for(let i = 0; i < CURSOR_COUNT; i++){
     cursors.push(new Cursor())
 }
+
+function animate(){
+    cursors.forEach(c => {
+        c.animate()
+    })
+
+    frameCount++
+    console.log(frameCount);
+    requestAnimationFrame(animate)
+}
+animate()
 
 let mousePos = null
 window.addEventListener('mousemove', e => {
@@ -61,7 +87,7 @@ window.addEventListener('mousemove', e => {
     const dy = prevPos.y - mousePos.y
 
     cursors.forEach(c => {
-        c.updatedPos(dx / 2, dy / 2);
+        c.updatedPos(dx / 2, dy / 2)
     })
 })
 
